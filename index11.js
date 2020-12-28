eb5b77f6eeb95cf390b4380e4ad6282cf4a0fcdb
Hong Phuc <147kinke@gmail.com>
2020-12-28 09:22:46 +0700

    add old file

---
+++ index11.js
@@ -0,0 +1,10 @@
+var x = document.addEventListener("DOMContentLoaded",function(){
+    //co 3 cach lay DOM
+    var x = document.getElementById('top-nav');
+    var y = document.getElementsByClassName('video-items');
+    var z = document.querySelector('.top-nav form');
+    var o = document.querySelectorAll('.col-lg-12');
+
+    //luu y
+    console.log(x);
+},false)
